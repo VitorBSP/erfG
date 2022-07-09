a8743a03a84738f3f9fe27613dc4477b16924a6c
VitorBSP <vitorpereira3115@gmai.com>
2022-07-09 04:53:35 -0300

    fix: remove "d" function

--- R/dexp.R
+++
@@ -1,6 +0,0 @@
-dexp <- function(x, alpha){
-  g = alpha*exp(-alpha*x)
-  G = 1- exp(-alpha*x)
-  f = 2*g*exp(-(G/(1-G))^2)/(sqrt(pi)*(1-G)^2)
-  f
-}
--- R/dnorm.R
+++
@@ -1,6 +0,0 @@
-dnorm <- function(x, mu, sigma){
-  g = dnorm(x, mean = mu, sd = sigma)
-  G = pnorm(x, mean = mu, sd = sigma)
-  f = 2*g*exp(-(G/(1-G))^2)/(sqrt(pi)*(1-G)^2)
-  f
-}
