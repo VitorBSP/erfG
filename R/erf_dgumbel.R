d90887f0a240d36d06849b629b0376f6ee3795e1
VitorBSP <vitorpereira3115@gmail.com>
2022-07-09 16:49:27 -0300

    Update R/erf_dgumbel.R
    Co-authored-by: AlissonRP <62574371+AlissonRP@users.noreply.github.com>

--- R/erf_dgumbel.R
+++ R/erf_dgumbel.R
@@ -1,4 +1,4 @@
-erf_dgumbel <- function(x, mu=0, sigma=1){
+erf_dgumbel <- function(x, mu = 0, sigma = 1){
   z = exp(-(x-mu)/sigma)
   f1 = 2*z*exp(-z - ((1)/(exp(z) - 1))^2)
   f2 = sqrt(pi)*sigma*((1 - exp(z))^2)
