6fd3a686b9291234b335e94a9d4df8c9c98280a0
VitorBSP <vitorpereira3115@gmail.com>
2022-07-09 16:47:41 -0300

    Update tests/testthat/test-erf_dgumbel.R
    Co-authored-by: AlissonRP <62574371+AlissonRP@users.noreply.github.com>

--- tests/testthat/test-erf_dgumbel.R
+++ tests/testthat/test-erf_dgumbel.R
@@ -2,7 +2,7 @@ x=rweibull(1,3)
 mu=rnorm(1)
 sigma=rweibull(1,1,3)
 test_that("erf_pnorm works", {
-  expect_true(erf_dgumbel(x=x,mu=mu,sigma=sigma)>=0)
+  expect_true(erf_dgumbel(x = x, mu = mu, sigma = sigma) >= 0)
   expect_true(erf_dgumbel(x=x,mu=mu,sigma=sigma)<=1)
   #integrate(erf_dgumbel, -Inf,Inf)
 })
