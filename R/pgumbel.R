03196e68958c6fc82a05f1f5db6bd7b7bce917e8
AlissonRP <alirpereira887@gmail.com>
2022-07-15 23:32:11 -0300

    fix in pgumbell

--- R/pgumbel.R
+++ R/pgumbel.R
@@ -1,10 +1,5 @@
 pgumbel <- function(x, mu, sigma){
-  if (rlang::is_installed("extraDistr")) {
-    G = extraDistr::pgumbel(x, mu = mu, sigma = sigma)
-    2*stats::pnorm((G/(1-G))*sqrt(2))-1
-  }else {
   z = exp(-(x-mu)/sigma)
   f = Erf(1/(exp(z) - 1))
-  }
   f
 }
